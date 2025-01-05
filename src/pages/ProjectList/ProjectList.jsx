2958a383d5786ed4a5240381ea7207a94e33cd48
karthikeyan <karthi2k415v@gmail.com>
2025-01-05 09:44:05 +0530

    work on ProjectList

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,13 +1,47 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { MixerHorizontalIcon } from '@radix-ui/react-icons'
 import React from 'react'
 
 const ProjectList = () => {
+  const handleFilterChange = (value) => {
+    console.log(value)
+  }
   return (
     <>
     <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
       <section className='filterSection'>
+        <Card className="p-5 sticky top-10 ">
 
+          <div className='flex justify-between lg:w-[20rem]'>
+            <p className='text-xl -tracking-wider'>filters</p>
+            <Button variant="ghost" size="icon">
+              <MixerHorizontalIcon/>
+            </Button>
+          </div>
+          <CardContent className="mt-5">
+            <ScrollArea className="space-y-7 h-[70vh]">
+              <div>
+                <h1 className='pb-3 text-gray-400 border-b'>
+                  Category
+                </h1>
+                <div className='pt-5'>
+                  <RadioGroup defaultValue="all" OnValueChange={(value)=>handleFilterChange("category",value)}>
+                    <div className='flex items-center gap-2'>
+                      <RadioGroupItem value="all" id="r1"/>
+                      <Label htmlFor="r1">all</Label>
+                    </div>
+                  </RadioGroup>
+                </div>
+              </div>
+            </ScrollArea>
+          </CardContent>
+        </Card>
       </section>
-      <section className='projectListSection'>
+      <section className='projectListSection w-full lg:w-[48rem]'>
         
       </section>
     </div>
