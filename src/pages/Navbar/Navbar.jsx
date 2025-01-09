26c97d9c605440070c846769f60db63039aba011
karthikeyan <karthi2k415v@gmail.com>
2025-01-09 12:30:17 +0530

    completed chatbox sections fully

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -5,13 +5,15 @@ import React from 'react'
 import CreateProjectForm from '../Project/CreateProjectForm'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { PersonIcon } from '@radix-ui/react-icons'
+import { useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div className='border-b py-4 px-5 flex items-center justify-between'>
         <div className='flex items-center gap-3'>
-            <p className='cursor-pointer'>
+            <p onClick={()=>navigate("/")} className='cursor-pointer'>
                 Project Management System
             </p>
             <Dialog>
--- src/pages/ProjectDetails/ChatBox.jsx
+++ src/pages/ProjectDetails/ChatBox.jsx
@@ -1,8 +1,21 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { ScrollArea } from '@/components/ui/scroll-area'
-import React from 'react'
+import { PaperPlaneIcon } from '@radix-ui/react-icons'
+import React, { useState } from 'react'
 
 const ChatBox = () => {
+  const [message , setMessage] = useState("");
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const handleSendMessage = () => {
+    console.log(message)
+    setMessage("")
+  }
   return (
     <div className='sticky'>
         <div className='border rounded-lg'>
@@ -11,7 +24,7 @@ const ChatBox = () => {
             {
               [1,1,1,1].map((item,index) => (
                 (index%2 == 0) ?(
-                <div className="flex gap-2 mb-2" key={item}>
+                <div className="flex gap-2 mb-2 justify-start" key={item}>
                   <Avatar>
                     <AvatarFallback>
                       K
@@ -22,21 +35,36 @@ const ChatBox = () => {
                       <p className='text-gray-300'>How are you</p>
                     </div>
                 </div>):(
-                  <div className="flex gap-2 mb-2 " key={item}>
-                  <Avatar>
+                  <div className="flex gap-2 mb-2 justify-end" key={item}>
+                    <div className='space-y-2 py-2 px-5  border rounded-se-2xl rounded-s-xl'>
+                      <p>Karthi</p>
+                      <p className='text-gray-300'>How are you</p>
+                      
+                    </div>
+                    <Avatar>
                     <AvatarFallback>
                       K
                     </AvatarFallback>
                   </Avatar>
-                    <div className='space-y-2 py-2 px-5  border rounded-se-2xl rounded-s-xl'>
-                      <p>Karthi</p>
-                      <p className='text-gray-300'>How are you</p>
-                    </div>
                 </div>
                 )
               ))
             }
           </ScrollArea>
+          <div className='relative p-0'>
+            <Input placeholder = "type message ..."
+              className="py-7 border-t outline-none focus:outline-none focus:ring-0 rounded-none border-b-0 border-x-0"
+              value={message}
+              onChange={handleMessageChange}
+            />
+            <Button 
+              onClick={handleSendMessage} 
+              className="absolute right-2 top-3 rounded-full" 
+              size="icon" 
+              variant = "ghost">
+                <PaperPlaneIcon/>
+            </Button>
+          </div>
         </div>
     </div>
   )
